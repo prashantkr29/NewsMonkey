3804f87e24c16bd70d9a0bb680a62f1b5b9590db
prashantkr29 <prashant.k2901@gmail.com>
2022-08-09 11:23:45 +0530

    routing to different categories

--- src/App.js
+++ src/App.js
@@ -4,12 +4,33 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News'
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 export default class App extends Component {
   render() {
     return (
       <>
+      
+      <BrowserRouter>
       <Navbar/>
-      <News/>
+      <Routes>
+       
+        <Route exact path='/science' element={<News key="science"category='science'/>}/>
+        <Route exact path='/health' element={<News key="health"category='health'/>}/>
+        <Route exact path='/technology' element={<News key="technology"category='Technology'/>}/>
+        <Route exact path='/entertainment' element={<News key="entertainment"category='Entertainment'/>}/>
+        <Route exact path='/sports' element={<News key="sports"category='Sports'/>}/>
+        <Route exact path='/politics' element={<News key="politics"category='Politics'/>}/>
+        <Route exact path='/buisness' element={<News key="buisness"category='buisness'/>}/>
+       
+
+      </Routes>
+      </BrowserRouter>
+     
       </>
     )
   }
--- src/components/News.js
+++ src/components/News.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-
+  
     constructor(){
         super();
         this.state = {
             articles: [],
             loading: false,
-            page:1
+            page:1,
+            pageSize:6
         }
     }
 
     async componentDidMount(){ 
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=dbe57b028aeb41e285a226a94865f7a7&page=1pageSize=20";
+        let url = `https://newsapi.org/v2/top-headlines?country='in'&category=${this.props.category}&apiKey=dbe57b028aeb41e285a226a94865f7a7&page=1pageSize=${this.state.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json()
         console.log(parsedData); 
@@ -21,8 +22,8 @@ export class News extends Component {
     }
 
      handlePrevClick = async ()=>{
-        console.log("Previous");
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page - 1}&pageSize=20`;
+        
+        let url = `https://newsapi.org/v2/top-headlines?country='in'&category=${this.props.category}&apiKey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page - 1}&pageSize=${this.state.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json()
         console.log(parsedData);  
@@ -34,12 +35,12 @@ export class News extends Component {
     }
     
      handleNextClick = async ()=>{
-        console.log("Next"); 
-        if (this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+        
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults/this.state.pageSize)){
 
         }
         else{
-            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page + 1}&pageSize=20`;
+            let url = `https://newsapi.org/v2/top-headlines?country='in'&category=${this.props.category}&apiKey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page + 1}&pageSize=${this.state.pageSize}`;
             let data = await fetch(url);
             let parsedData = await data.json()
             console.log(parsedData);  
@@ -51,9 +52,11 @@ export class News extends Component {
     }
 
     render() { 
+   
         return (
+          
             <div className="container my-3">
-                <h1>NewsMonkey - Top Headlines</h1> 
+                <h1 style={{textAlign:'center'}}>NewsMonkey - Top Headlines</h1> 
                 <div className="row"> 
                 {this.state.articles.map((element)=>{
                     return <div className="col-md-4" key={element.url}>
